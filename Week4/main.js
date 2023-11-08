36080c3184617133c08c50d332271dcd44454544
Matthew <mb2095@hw.ac.uk>
2023-11-09 00:00:36 +0400

    Saved

--- Week4/main.js
+++ Week4/main.js
@@ -1,6 +1,7 @@
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://github.com/MatthewBerry1/MatthewBerry1.github.io/blob/main/Week4/cities1.json');
 ourRequest.onload = function() {
-console.log(ourRequest.responseText);
-}
-ourRequest.send();
+    console.log(ourRequest.responseText);
+    };
+    ourRequest.send();
+    
