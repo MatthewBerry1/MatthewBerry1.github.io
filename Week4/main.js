75d1a04fbd7fa2890c43afbd18ed0f0ef39f16a9
Matthew <mb2095@hw.ac.uk>
2023-11-08 23:42:57 +0400

    Added link for main.js file

--- Week4/main.js
+++ Week4/main.js
@@ -1,5 +1,5 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://MatthewBerry1.github.io/MatthewBerry1/cities1.json');
+ourRequest.open('GET', 'https://github.com/MatthewBerry1/MatthewBerry1.github.io/blob/main/Week4/cities1.json');
 ourRequest.onload = function() {
 console.log(ourRequest.responseText);
 }
