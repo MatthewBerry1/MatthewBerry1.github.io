5d7d448e87ac24cb3f8df46ba0c1151563ec5f0f
Matthew <mb2095@hw.ac.uk>
2023-11-22 23:31:07 +0400

    lab5

--- Week5/authentication/index.js
+++ Week5/authentication/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 5000;
 app.listen(port, () => {
     console.log(`Server is live on port ${port}`);
 });
